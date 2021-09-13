dd8828f7cc696ca2b86ff5a02f0e935b51810547
Jcoder2022 <JCoder2022@gmail.com>
2021-09-13 13:32:04 +0100

    ADDED: Number opertaions

---
+++ app.py
@@ -0,0 +1,11 @@
+# Number
+print(10*3)
+print(10+3)
+print(10-3)
+# Two types of division
+print(10/3) # returns floating point number
+print(10//3) # returns int
+print(10%3)# mod return remainder
+# exponent **
+print (10 ** 3) # will return 1000
+
